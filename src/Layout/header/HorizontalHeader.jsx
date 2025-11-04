7dd0cb64ebd1b8b06b84dbaa757640df6e208c7e
Thanh The <thengo2108@gmail.com>
2025-11-04 21:09:01 +0700

    Init

---
+++ src/Layout/header/HorizontalHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HorizontalHeader = () => {
+    return (
+        <h1>Header ne</h1>
+    )
+}
+
+export default HorizontalHeader;
---
+++ src/Layout/header/VerticalHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VerticalHeader = () => {
+    return (
+        <h1>Header doc ne</h1>
+    )
+}
+
+export default VerticalHeader;
